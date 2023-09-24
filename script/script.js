e5e8e6f214791d17624406ec3961f5e401b3c951
maxiderecho <maxi.derecho@gmail.com>
2023-09-24 18:59:26 -0300

    [FEAT] Agrego filtro desplegable en versión mobile en la página del shop.

--- script/script.js
+++ script/script.js
@@ -1,8 +1,17 @@
-//  Submenú desplegable para versión mobile.
+//  Menú hamburguesa en el navbar del header para versión mobile.
 
 const navbarIcon = document.querySelector(".navbar__icon");
 const navbarMenu = document.querySelector(".navbar__menu");
 
 navbarIcon.addEventListener("click", () => {
   navbarMenu.classList.toggle("show-menu");
+});
+
+//  Filtro desplegable en el shop para versión mobile.
+
+const filterButton = document.querySelector(".filter__button");
+const filterContent = document.querySelector(".filter__content--desplegable");
+
+filterButton.addEventListener("click", () => {
+  filterContent.classList.toggle("filter-show");
 });
