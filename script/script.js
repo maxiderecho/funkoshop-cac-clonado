86c4527eba616e91cf7f3a32072745e04f8445d8
maxiderecho <maxi.derecho@gmail.com>
2023-09-24 19:52:07 -0300

    Agrego rotación de 180° en el ícono chevron down en el botón de filtros para versión mobile.

--- script/script.js
+++ script/script.js
@@ -11,7 +11,9 @@ navbarIcon.addEventListener("click", () => {
 
 const filterButton = document.querySelector(".filter__button");
 const filterContent = document.querySelector(".filter__content--desplegable");
+const filterChevron = document.querySelector(".filter__button--icon");
 
 filterButton.addEventListener("click", () => {
   filterContent.classList.toggle("filter-show");
+  filterChevron.classList.toggle("filter__button--rotate");
 });
