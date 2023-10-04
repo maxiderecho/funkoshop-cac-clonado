8ea663d5c803844fba53c75d476ada33762bcf48
maxiderecho <maxi.derecho@gmail.com>
2023-10-03 23:51:08 -0300

    [FEAT] Agrego estilos versión mobile para admin.html. Correción de comentarios en script.js

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-//  Menú hamburguesa en el navbar del header para versión mobile.
+//  Menú hamburguesa despegable en el navbar del header para versión mobile.
 
 const navbarIcon = document.querySelector(".navbar__icon");
 const navbarMenu = document.querySelector(".navbar__menu");
@@ -6,6 +6,8 @@ const navbarMenu = document.querySelector(".navbar__menu");
 navbarIcon.addEventListener("click", () => {
   navbarMenu.classList.toggle("show-menu");
 });
+
+// Agregamos que ante cualquier scroll en la página el menú hamburguesa se oculte.
 
 document.addEventListener("scroll", () => {
   navbarMenu.classList.remove("show-menu");
